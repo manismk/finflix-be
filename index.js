c33b9d1ace93fc7fddcbf5b03725f828b79d32b4
Manikandan S <manismkmech@gmail.com>
2024-01-27 14:41:43 +0530

    feat: add global catch and body parsing middlewares

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.get("/", (req, res) => {
   res.send("Hello Finflix!");
 });
 
+// Middleware to parse body
+app.use(express.json());
+
+// Global catch, Server errors
+app.use((err, req, res, next) => {
+  // TODO Move this to logger later
+  console.log("err", err);
+  res.status(500).json({
+    message: "Something is went wrong",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
