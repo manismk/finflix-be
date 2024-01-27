61785789c32c7a6212c094c658c515817feffb21
Manikandan S <manismkmech@gmail.com>
2024-01-27 13:34:01 +0530

    chore: intial express setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello finflix");
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+dotenv?.config();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello Finflix!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
